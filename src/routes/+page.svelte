3c091cc8ea9fdd898c013e2fc7fa2bdcc1e4b978
Sojty <sojak.vit@gmail.com>
2025-08-17 17:56:04 +0200

    fix: a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
 <h1>GLAPKO Studios</h1>
-<p>Visit <a href="/about">svelte.dev/docs/kit</a> to read the documentation :)</p>
+<p>
+  Visit <a href="{base}/about">svelte.dev/docs/kit</a> to read the documentation :)
+</p>
